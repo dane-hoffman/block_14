ed408536e2baf54e1385f2d4e8ef5aee74c5c94f
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-10-26 13:38:57 -0600

    Transform the original array

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
+//Create an array name myInstruments that contains the following instruments: flute, clarinet, saxophone, trumpet, and trombone.
 myInstruments = [`saxophone`, `flute`, `clarinet`, `trumpet`, `trombone`];
+
+//Write code to transform it to include tuba, and bassoon.
+
+myInstruments.push (`tuba`,`bassoon`);
 console.log(myInstruments);
